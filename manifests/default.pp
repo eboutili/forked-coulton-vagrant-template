79ac749789895eb8bf2c73796e5eb28e59b85745
eboutili <eric.boutilier@puppet.com>
2017-05-31 10:07:23 -0500

    Comments, requires, and preferences

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,17 @@
 node 'node-01' {
-include docker
-include docker::compose
+   include 'docker'
+#  include 'docker::compose'    # Redundant: docker::compose class is redundant (completely, I think) when using docker::run below.
+
+  docker::image { 'mynginx_i':
+    ensure      => 'present',
+    docker_dir => '/vagrant',   # Image assembly comes from the Dockerfile in this directory
+    require => Class['docker'],
+  }
+
+  docker::run { 'mynginx_c':
+    image    => 'nginx',
+    ports    => '80:80',        # Don't define ports using array any more
+    hostname =>  'nginx',
+    require => Docker::Image['mynginx_i'],
+  }
 }
-#class {'docker::compose': ensure => present, }
